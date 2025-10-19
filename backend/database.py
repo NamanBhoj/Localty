3022cbb11a67a75fc8ce4d306200de58e2279ff0
Naman Bhoj <namanbhoj99@gmail.com>
2025-10-19 13:28:11 -0600

    defined models first then database and added dependencies of dotnev and psycopg2

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,26 @@
 # db logic
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+DATABASE_URI = os.getenv("DATABASE_URI")
+
+if not DATABASE_URI:
+    print("URI Issues")
+
+db_engine = create_engine(DATABASE_URI)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+Base = declarative_base()
+
+
+def create_db():
+    from backend.database import Base, engine
+    from backend import models
+
+    print("Creating")
+    Base.metadata.create_all(bind=db_engine)
+    print("Creatied")
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,23 @@
 # data models
+
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from backend.database import Base
+from datetime import datetime 
+
+
+class Transaction_Record(Base):
+    __tablename__ = "transaction_record"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String)
+    amount = Column(Float)
+    category = Column(String)
+    timestamp = Column(DateTime)
+
+class Loyalty_Record(Base):
+    __tablename__ = "loyalty_record"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String)
+    points = Column(Float)
+
+
